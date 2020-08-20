04b4293ae02126d87c05b30248f6f770df870520
Caio Willian <cwillian40@hotmail.com>
2020-08-19 23:31:38 -0300

    [#04 - 40:36] Incluindo o boiler plate do VUE no projeto

---
+++ vuejs-cli/src/components/Comments.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div class="form-todo form-group">
+      <h1>Comentários</h1>
+      <hr>
+      <input type="text" placeholder="nome" name="author" v-model="name" class="form-control">
+      <br>
+      <textarea placeholder="Comentário" name="message" v-model="message" class="form-control"></textarea>
+      <br>
+      <button v-on:click="addComment" type="submit" class="btn btn-primary">Comentar</button>
+    </div>
+    <div class="list-group">
+      <div class="list-group-item" v-bind:key=index v-for="(comment, index) in allComments">
+        <span class="comment__author">Autor: <strong>{{comment.name}}</strong></span>
+        <p>{{comment.message}}</p>
+        <div>
+          <a href="#" title="Excluir" v-on:click.prevent="removeComments(index)">Excluir</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      comments: [],
+      name: '',
+      message: '',
+    }
+  },
+  methods: {
+    addComment() {
+      if (this.message.trim() === '') {
+        return
+      }
+      this.comments.push({
+        name: this.name,
+        message: this.message
+      })
+
+      this.name = ''
+      this.message = ''
+    },
+    removeComments (index) {
+      this.comments.splice(index, 1)
+    }
+  },
+  computed: {
+    allComments() {
+      return this.comments.map(comment => ({
+        ...comment,
+        name: comment.name.trim() === '' ? 'Anônimo' : comment.name
+      }))
+    }
+  },
+  watch: {
+    comments(val) {
+      console.log('val', val)
+    }
+  }
+}
+</script>
